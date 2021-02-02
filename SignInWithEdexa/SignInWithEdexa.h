86c413c61fb14c4287cc220bf1a73aa769bd8b87
Sachin Patoliya <s.patoliya@edexa.team>
2021-02-02 16:04:05 +0530

    First Commit

---
+++ SignInWithEdexa/SignInWithEdexa.h
@@ -0,0 +1,19 @@
+//
+//  SignInWithEdexa.h
+//  SignInWithEdexa
+//
+//  Created by Edexa_IOS on 02/02/21.
+//  Copyright © 2021 Edexa_IOS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SignInWithEdexa.
+FOUNDATION_EXPORT double SignInWithEdexaVersionNumber;
+
+//! Project version string for SignInWithEdexa.
+FOUNDATION_EXPORT const unsigned char SignInWithEdexaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignInWithEdexa/PublicHeader.h>
+
+
